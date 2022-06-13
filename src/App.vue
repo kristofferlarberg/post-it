0006a9ccada4a409b497c770a69ec703c3721d1d
Kristoffer Larberg <mail@kristofferlarberg.se>
2022-06-13 17:38:30 +0200

    chore: initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { RouterView } from "vue-router";
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">post-it</div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style>
+@import "@/assets/base.css";
+
+#app {
+  padding: 2rem;
+
+  font-weight: normal;
+}
+
+@media (hover: hover) {
+}
+</style>
