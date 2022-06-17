2e93c7dea991c906602e0d90e55cebddd166ad1e
Kristoffer Larberg <mail@kristofferlarberg.se>
2022-06-17 10:23:20 +0200

    feat: functionality for adding and deleting notes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,40 @@
-<script setup></script>
+<script>
+let id = 0;
+
+export default {
+  data() {
+    return {
+      newNote: "",
+      notes: [
+        { id: id++, text: "Note" },
+        { id: id++, text: "Note again" },
+        { id: id++, text: "Note some more" },
+      ],
+    };
+  },
+  methods: {
+    addNote() {
+      this.notes.push({ id: id++, text: this.newNote, done: false });
+      this.newNote = "";
+    },
+    removeNote(note) {
+      this.notes = this.notes.filter((t) => t !== note);
+    },
+  },
+};
+</script>
 
 <template>
-  <main>Hello</main>
+  <main>
+    <ul>
+      <li v-for="note in notes" :key="note.id">
+        {{ note.text }}
+        <button @click="removeNote(note)">X</button>
+      </li>
+    </ul>
+    <form @submit.prevent="addNote">
+      <input v-model="newNote" />
+      <button>Add note</button>
+    </form>
+  </main>
 </template>
