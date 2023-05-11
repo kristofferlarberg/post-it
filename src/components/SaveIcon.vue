eccb3b6e95d3c1b165faf8c540c09960d98a3b13
kristofferlarberg <kristoffer.larberg@icloud.com>
2023-05-11 15:54:05 +0200

    refactor: new edit text icon

--- src/components/SaveIcon.vue
+++ src/components/SaveIcon.vue
@@ -2,10 +2,11 @@
   <svg
     xmlns="http://www.w3.org/2000/svg"
     class="stroke-violet stroke-[3px] hover:stroke-white-secondary"
-    viewBox="-1 -1 55 55"
+    viewBox="-5 -3 55 55"
     fill="none"
     aria-hidden="true"
   >
-    <path d="M1 26.7009L26.463 51L51 1" />
+    <path d="M20 25.3645L33.2407 38L46 12" />
+    <path d="M0 49H12.5M25 49H12.5M12.5 49V1M0 1H25" />
   </svg>
 </template>
