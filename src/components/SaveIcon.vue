205ca6f27c2965ab21d47ab347dd5db27f675196
kristofferlarberg <kristoffer.larberg@icloud.com>
2023-05-17 20:17:57 +0200

    fix: optimize note menu visibility in dark mode

--- src/components/SaveIcon.vue
+++ src/components/SaveIcon.vue
@@ -1,7 +1,7 @@
 <template>
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    class="stroke-violet stroke-[3px] hover:stroke-white-secondary"
+    class="stroke-violet stroke-[3px]"
     viewBox="-5 -3 55 55"
     fill="none"
     aria-hidden="true"
