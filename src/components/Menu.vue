80d0c7844053266e87e67002202ac5b8afe142eb
kristofferlarberg <kristoffer.larberg@icloud.com>
2023-05-29 15:50:51 +0200

    refactor: create component for menu buttons

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -4,6 +4,7 @@ import SaveTextIcon from "./icons/SaveTextIcon.vue";
 import EditTextIcon from "./icons/EditTextIcon.vue";
 import ImageIcon from "./icons/ImageIcon.vue";
 import DeleteContentIcon from "./icons/DeleteContentIcon.vue";
+import MenuButton from "./MenuButton.vue";
 
 const props = defineProps({
   showMenu: { type: Boolean, default: false },
@@ -22,51 +23,42 @@ defineEmits(["handleImageInput", "handleTextInput", "resetNote", "removeNote"]);
     ]"
   >
     <div class="flex gap-3">
-      <button
+      <MenuButton
         v-if="props.contentType !== 'text'"
-        class="h-[30px] w-[30px]"
         aria-label="Add image to note"
-        @click="$emit('handleImageInput')"
+        @handle-click="$emit('handleImageInput')"
       >
         <ImageIcon />
-      </button>
-      <button
+      </MenuButton>
+      <MenuButton
         v-if="
           (props.contentType !== 'image' && active) || (!contentType && active)
         "
-        class="h-[30px] w-[30px]"
         aria-label="Save note"
-        @click="$emit('handleTextInput')"
+        @handle-click="$emit('handleTextInput')"
       >
         <SaveTextIcon v-if="active" />
-      </button>
-      <button
+      </MenuButton>
+      <MenuButton
         v-if="
           (props.contentType !== 'image' && !active) ||
           (!contentType && !active)
         "
-        class="h-[30px] w-[30px]"
-        aria-label="Edit note"
-        @click="$emit('handleTextInput')"
+        aria-label="Edit note text"
+        @handle-click="$emit('handleTextInput')"
       >
         <EditTextIcon />
-      </button>
+      </MenuButton>
     </div>
-
-    <button
+    <MenuButton
       v-if="props.contentType"
-      class="h-[30px] w-[30px]"
-      aria-label="Remove image from note"
-      @click="$emit('resetNote')"
+      aria-label="Remove content from note"
+      @handle-click="$emit('resetNote')"
     >
       <DeleteContentIcon />
-    </button>
-    <button
-      class="h-[30px] w-[30px]"
-      aria-label="Remove note"
-      @click="$emit('removeNote')"
-    >
+    </MenuButton>
+    <MenuButton aria-label="Remove note" @handle-click="$emit('removeNote')">
       <DeleteIcon class="h-[30px] w-[30px]" />
-    </button>
+    </MenuButton>
   </div>
 </template>
---
+++ src/components/MenuButton.vue
@@ -0,0 +1,17 @@
+<script setup>
+const props = defineProps({
+  ariaLabel: { type: String, default: "" },
+});
+
+defineEmits(["handleClick"]);
+</script>
+
+<template>
+  <button
+    class="h-[30px] w-[30px]"
+    :aria-label="props.ariaLabel"
+    @click="$emit('handleClick')"
+  >
+    <slot />
+  </button>
+</template>
